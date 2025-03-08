09a4e8907e8772155cf800f5b3faf8d4b3ba714f
ah1028 <amyhersey04@gmail.com>
2025-03-08 20:54:09 +0000

    Added login auth

--- Combining/dopamine_menu.py
+++ Combining/dopamine_menu.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
+from flask import Flask, render_template, request, url_for, flash, redirect, abort, session
+import db_functions as db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if db.login(username, password):
+        session['user'] = username 
+        return redirect(url_for('menupage'))
+    else:
+        flash("Invalid username or password", "danger")
+        return redirect(url_for('index'))  # Reload login page with flash message
 
 @app.route('/menupage/')
 def menupage():
