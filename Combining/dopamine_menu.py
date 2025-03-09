6235067b7ba857ea252fb44bc11afa8241299d50
ah1028 <amyhersey04@gmail.com>
2025-03-09 01:55:03 +0000

    Fixed form responses

--- Combining/dopamine_menu.py
+++ Combining/dopamine_menu.py
@@ -24,14 +24,18 @@ def login():
 def menupage():
     return render_template('menupage.html')
 
-@app.route('/new-activity/')
+@app.route('/new-activity/', methods=('GET', 'POST'))
 def new_activity():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['description']
+        exp = request.form['expense']
+        tod = request.form['tod'] # This is value A, B4, AD
+        mini = request.form['mini']
+        maxi = request.form['maxi']
+        tags = request.form.getlist('exercise') + request.form.getlist('art') + request.form.getlist('mindfullness')
+        if not title:
+            flash('Title is required!')
+        else:
+            return redirect(url_for('menupage'))
     return render_template('new-activity.html')
-
-@app.route('/submit-activity', methods=['POST'])
-def submit_activity():
-    image = request.form['image']
-    print("hi")
-    print(image)
-    if True:
-        return redirect(url_for('menupage'))
